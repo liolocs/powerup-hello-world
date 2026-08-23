ccd49982c51d969f5be4e7a3a234c249ba382179
Olivier Colas <olivercolas.oc@gmail.com>
2026-08-23 10:36:40 +0200

    refactor: pkg json

--- src/packageJson.ts
+++ src/packageJson.ts
@@ -9,15 +9,6 @@ export default function(variables: Record<string, string>): string {
     scripts: {
       dev: "npx tsx index.ts",
     },
-    keywords: ["powerups-package"],
-    powerup: { instructions: "index.ts", compatibility: {} },
-    files: ["dist"],
-    exports: {
-      ".": {
-        import: "./dist/index.js",
-        types: "./dist/index.d.ts",
-      },
-    },
   };
 
   return JSON.stringify(pkg, null, 2) + "\n";
